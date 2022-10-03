1f971c34a56e59215b5867efba930afadfc3ca99
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-10-03 15:33:21 +0300

    create footer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 
 function App() {
   return (
-   <header/ >
+     <Header/>
+       <main>
+          <Routes>
+              
+          </Routes>
+       </main>
+     <Footer/>
+
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <>
+            <footer id="sticky-footer" className="bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">Copyright © Shared Trip 2021</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
