fb7c6efe174dae8daef31913f0b38c9aa5fdd947
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2022-10-03 15:35:17 +0300

    Create home component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
      <Header/>
        <main>
           <Routes>
-              
+               <Route path='/' element={(<Home />)} />
           </Routes>
        </main>
      <Footer/>
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,21 @@
+const Home = () => {
+    return (
+        <section id="home-page" className="py-5">
+            <div style={{ height: "15vh" }} />
+            <div className="container home">
+                <h1>Shared Trip</h1>
+                <div>
+                    <p className="lead">
+                        "Because the greatest part of a road trip isn't arriving at your
+                        destination."
+                    </p>
+                    <p className="lead">
+                        "It's all the wild stuff that happens along the way..."
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
