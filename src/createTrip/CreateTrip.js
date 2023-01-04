e36add01de6626138848db87ea2ee5bfa9aeb043
Tihomir Zhelyazkov <88166066+tihomirtx88@users.noreply.github.com>
2023-01-04 15:57:33 +0200

    changes

--- src/createTrip/CreateTrip.js
+++ src/createTrip/CreateTrip.js
@@ -60,7 +60,7 @@ const CreateTrip = () => {
     const createSchema = Yup.object().shape({
         start: Yup.string().min(4, "Too Short!").max(10, "Too Long!").required("Required!"),
         end: Yup.string().min(4, "Too Short!").max(10, "Too Long!").required("Required!"),
-        carImg: Yup.string().email("Must be valid URL").required("Required!"),
+        carImg: Yup.string().required("Required!"),
         carBrand: Yup.string().min(4, "Too Short!").max(10, "Too Long!").required("Required!"),
         price: Yup.number().min(1, "Too Short!").max(51, "Too Long!").required("Required!"),
         seats: Yup.number().min(0, "Too Short!").max(5, "Too Long!").required("Required!"),
